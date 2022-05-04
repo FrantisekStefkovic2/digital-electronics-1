1d4c35262c5594f382980512c0eecd71cf107f17
student <student@PC-630.urel.feec.vutbr.cz>
2022-05-04 14:04:03 +0200

    d

---
+++ labs/05-ffp/05-ffp.srcs/sim_1/new/t_ff_rst.vhd
@@ -0,0 +1,49 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity d_ff_rst is
+    Port (clk   : in STD_LOGIC;
+          rst   : in STD_LOGIC;
+          d   : in STD_LOGIC;
+          q   : out STD_LOGIC;
+          q_bar   : out std_logic
+          );
+--  Port ( );
+end d_ff_rst;
+
+
+
+
+architecture Behavioral of d_ff_rst is
+begin
+    --------------------------------------------------------
+    -- p_d_ff_rst:
+    -- D type flip-flop with a high-active sync reset,
+    -- rising-edge clk.
+    -- q(n+1) = d
+    --------------------------------------------------------
+    p_d_ff_rst : process(clk)
+    begin
+        if rising_edge(clk) then  -- Synchronous process
+            if (rst = '1') then
+                q <= '0';
+                q_bar <= '1';
+                q_n <= '0'; 
+            -- USE HIGH-ACTIVE RESET HERE
+            
+            else 
+                q_n <= (not())
+                q   <= ;
+                q_bar <= not ;
+        end if;
+    end process p_d_ff_rst;
+end architecture Behavioral;
---
+++ labs/05-ffp/05-ffp.srcs/sources_1/new/ffs.vhd
@@ -0,0 +1,69 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 03/16/2022 01:23:53 PM
+-- Design Name: 
+-- Module Name: ffs - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity d_ff_rst is
+    Port (clk   : in STD_LOGIC;
+          rst   : in STD_LOGIC;
+          d   : in STD_LOGIC;
+          q   : in STD_LOGIC;
+          q_bar   : in STD_LOGIC);
+--  Port ( );
+end d_ff_rst;
+
+architecture Behavioral of d_ff_rst is
+
+begin
+    --------------------------------------------------------
+    -- p_t_ff_rst:
+    -- T type flip-flop with a high-active synchro reset,
+    -- rising-edge clk.
+    -- q(n+1) = t./q(n) + /t.q(n)
+    -- q(n+1) =  q(n) if t = 0 (no change)
+    -- q(n+1) = /q(n) if t = 1 (inversion)
+    --------------------------------------------------------
+    p_t_ff_rst : process(clk)
+    begin
+        if rising_edge(clk) then
+            if (rst = '1') then
+                q <= '0';
+                q_bar <= '1';
+            else
+                q <= d;
+                q_bar <= not d;
+            end if;
+        end if;
+    end process p_d_ff_rst;
+end architecture Behavioral;
+          
+        -- WRITE YOUR CODE HERE
+
